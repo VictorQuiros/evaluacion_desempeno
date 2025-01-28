298981fca6b6a419e7f87afcc87ee4ee5e110ee0
VictorQuiros <vquipav1505@g.educaand.es>
2025-01-28 09:29:25 +0100

    Editado archivo gestion_evaluacion_desempeno.py

---
+++ models/gestion_evaluacion_desempeno.py
@@ -0,0 +1,46 @@
+# evaluacion_desempeno/models/gestion_evaluacion_desempeno.py
+
+from odoo import models, fields, api
+
+
+class EvaluacionDesempeno(models.Model):
+    _name = 'gestion.desempeno'
+    _description = 'Evaluación del Empleado'
+
+    name = fields.Char(string='Título de la Evaluación', required=True)
+    comentarios = fields.Text(string='Comentarios del Evaluador')
+    puntuacion = fields.Integer(string='Puntuación', default=1, required=True)
+    assigned_to = fields.Many2one('hr.employee', string='Empleado Asignado')
+    evaluation_date = fields.Date(string='Fecha de Evaluación')
+
+    state = fields.Selection([
+        ('pending', 'Pendiente'),
+        ('in_progress', 'En Proceso'),
+        ('completed', 'Finalizado'),
+    ], string='Estado', default='pending')
+
+
+    icon = fields.Char(string='Icono del Módulo')
+
+    @api.model
+    def create(self, vals):
+
+        if not self.env.user.has_group('hr.group_hr_manager'):
+            return super(EvaluacionDesempeno, self).create(vals)
+
+
+    def write(self, vals):
+
+        if not self.env.user.has_group('hr.group_hr_manager'):
+            return super(EvaluacionDesempeno, self).write(vals)
+
+    def unlink(self):
+
+        if self.env.user.has_group('hr.group_hr_manager'):
+            return super(EvaluacionDesempeno, self).unlink()
+
+
+    @api.onchange('state')
+    def _onchange_state(self):
+        if self.state == 'completed':
+            self.priority = '0'  # Baja prioridad si está completada
