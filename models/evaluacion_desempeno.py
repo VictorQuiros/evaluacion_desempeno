b40b3bc7e811a5976534dc93114502e343fd4808
VictorQuiros <vquipav1505@g.educaand.es>
2025-02-04 09:06:38 +0100

    Corrección de errores

--- models/evaluacion_desempeno.py
+++ models/evaluacion_desempeno.py
@@ -1,46 +1,31 @@
-# evaluacion_desempeno/models/gestion_evaluacion_desempeno.py
-
 from odoo import models, fields, api
 
-
 class EvaluacionDesempeno(models.Model):
-    _name = 'gestion.desempeno'
-    _description = 'Evaluación del Empleado'
+    _name = 'evaluacion.desempeno'
+    _description = 'Evaluación de Desempeño'
 
     name = fields.Char(string='Título de la Evaluación', required=True)
     comentarios = fields.Text(string='Comentarios del Evaluador')
-    score = fields.Date(string = 'Puntuación', required=True, defult=1, help="Puntuación del 1 al 10", domain = [('score', '>=', 1), ('score', '<=', 10)])
+    score = fields.Float(string='Puntuación', required=True, default=1.0, help="Puntuación del 1 al 10")
     empleadoAsignado = fields.Many2one('hr.employee', string='Empleado Asignado')
     fechaEvaluacion = fields.Date(string='Fecha de Evaluación')
 
     state = fields.Selection([
-        ('pending', 'Pendiente'),
+        ('draft', 'Borrador'),
         ('in_progress', 'En Proceso'),
-        ('completed', 'Finalizado'),
-    ], string='Estado', default='pending')
+        ('done', 'Hecho'),
+    ], string='Estado', default='draft')
 
-
+    priority = fields.Integer(string='Prioridad', default=1)
     icon = fields.Char(string='Icono del Módulo')
 
-    @api.model
-    def create(self, vals):
+    def iniciar_evaluacion(self):
+        self.state = 'in_progress'
 
-        if not self.env.user.has_group('hr.group_hr_manager'):
-            return super(EvaluacionDesempeno, self).create(vals)
-
-
-    def write(self, vals):
-
-        if not self.env.user.has_group('hr.group_hr_manager'):
-            return super(EvaluacionDesempeno, self).write(vals)
-
-    def unlink(self):
-
-        if self.env.user.has_group('hr.group_hr_manager'):
-            return super(EvaluacionDesempeno, self).unlink()
-
+    def finalizar_evaluacion(self):
+        self.state = 'done'
 
     @api.onchange('state')
     def _onchange_state(self):
         if self.state == 'completed':
-            self.priority = '0'  # Baja prioridad si está completada
+            self.priority = 0  # Baja prioridad si está completada
